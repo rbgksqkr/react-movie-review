a5f352efd0383fff6e6f1bdbc1a5482310563726
rbgksqkr <rbgks1937@naver.com>
2024-06-11 15:33:09 +0900

    feat: 버튼 클릭 시 다음 페이지 데이터 불러오기

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
 import "./App.module.css";
-import { useEffect, useState } from "react";
-import { fetchMovieList } from "./api/movie";
-import { Movie } from "./types/movie";
+
 import MovieCardList from "./components/movieCardList/MovieCardList";
 import Header from "./components/header/Header";
 
 const App = () => {
-  const [movieList, setMovieList] = useState<Movie[]>([]);
-
-  useEffect(() => {
-    fetchMovieList().then((res) => {
-      setMovieList(res);
-    });
-  }, []);
-
   return (
     <>
       <Header />
       <main>
-        <MovieCardList movieList={movieList} />
+        <MovieCardList />
       </main>
     </>
   );
--- src/components/movieCardList/MovieCardList.tsx
+++ src/components/movieCardList/MovieCardList.tsx
@@ -1,14 +1,25 @@
 import styles from "./MovieCardList.module.css";
 import { Movie } from "../../types/movie";
 import MovieCard from "../movieCard/MovieCard";
-
-interface MovieCardListProps {
-  movieList: Movie[];
-}
+import { fetchMovieList } from "../../api/movie";
+import { useEffect, useState } from "react";
 
 const IMAGE_BASE_URL = import.meta.env.VITE_IMAGE_BASE_URL;
 
-const MovieCardList = ({ movieList }: MovieCardListProps) => {
+const MovieCardList = () => {
+  const [movieList, setMovieList] = useState<Movie[]>([]);
+  const [page, setPage] = useState(1);
+
+  const handleClickLoadMore = () => {
+    setPage((prev) => prev + 1);
+  };
+
+  useEffect(() => {
+    fetchMovieList(page).then((res) => {
+      setMovieList((prevMovieList) => [...prevMovieList, ...res]);
+    });
+  }, [page]);
+
   return (
     <section className={styles.itemView}>
       <h2>지금 인기 있는 영화</h2>
@@ -22,7 +33,10 @@ const MovieCardList = ({ movieList }: MovieCardListProps) => {
           />
         ))}
       </ul>
-      <button className={`${styles.btn} ${styles.primary} ${styles.fullWidth}`}>
+      <button
+        className={`${styles.btn} ${styles.primary} ${styles.fullWidth}`}
+        onClick={handleClickLoadMore}
+      >
         더 보기
       </button>
     </section>
